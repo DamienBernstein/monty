9eddda3d17e99b0bb1e88fe5dba32cb36108507f
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-12-14 11:55:43 +0200

    Update monty.c

--- monty.c
+++ monty.c
@@ -99,7 +99,7 @@ int main(int argc, char *argv[])
 	*stack = NULL;
 	line_number = 1;
 	
-	if (argc < 2 || agrc > 2)
+	if (argc < 2 || argc > 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
